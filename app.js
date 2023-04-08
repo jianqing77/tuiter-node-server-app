2157ed8238129fe79c258d35a1ae996348e3f696
Jianiqng Ma <jianqing997@gmail.com>
2023-04-08 19:03:47 -0400

    a9 Mongo sp23

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ import HelloController from './controllers/hello-controller.js';
 import UserController from './controllers/users/users-controller.js';
 import TuitsController from './controllers/tuits/tuits-controller.js';
 
+// load the mongoose library
+import mongoose from 'mongoose';
+
+const CONNECTION_STRING =
+    process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+// connect to the local host machine tuiter database
+mongoose.connect(CONNECTION_STRING);
+
 // execute the request and save return value to a var app
 const app = express();
 // configure cors right after instantiating express
--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,49 +1,54 @@
-import posts from './tuits.js';
-
-let tuits = posts;
+import * as tuitsDao from '../../tuits/tuits-dao.js';
+// import posts from './tuits.js';
+// let tuits = posts;
 
 /**
  * create a new tuit based on the request body,
  * and then add it to the tuits array
  * http://localhost:4000/api/tuits
  */
-const createTuit = (req, res) => {
+const createTuit = async (req, res) => {
     const newTuit = req.body; // extract new user from BODY in the request
-    newTuit._id = new Date().getTime() + '';
+    // newTuit._id = new Date().getTime() + ''; // ID created by database instead
     newTuit.likes = 0;
     newTuit.liked = false;
-    tuits.push(newTuit); // append new Tuit to the tuit array
-    res.json(newTuit); // respond new Tuit json to the client
+    // tuits.push(newTuit); // append new Tuit to the tuit array // not using array anymore
+    const insertedTuit = await tuitsDao.createTuit(newTuit); // actual tuit inserted in database with DAO's createTuit
+    res.json(insertedTuit); // respond new Tuit json to the client
 };
 
 /**
  * Return all the tuits in the tuits array.
  * http://localhost:4000/api/tuits
  */
-const findTuits = (req, res) => {
-    res.json(tuits);
+const findTuits = async (req, res) => {
+    const tuits = await tuitsDao.findTuits(); // now it's asynchronous function
+    res.json(tuits); // retrieve tuits from database
 };
 
 /**
  * update the tuit with the specified tid parameter in the URL
  * based on the request body.
  */
-const updateTuit = (req, res) => {
+const updateTuit = async (req, res) => {
     const tarTuitId = req.params.tid;
     const updateContent = req.body;
     // tuits = tuits.map((t) => (t._id === tarTuitId ? { ...t, ...updateContent } : t));
-    const tarIndex = tuits.findIndex((t) => t._id === tarTuitId);
-    tuits[tarIndex] = { ...tuits[tarIndex], ...updateContent };
-    res.sendStatus(200);
+    // const tarIndex = tuits.findIndex((t) => t._id === tarTuitId);
+    // tuits[tarIndex] = { ...tuits[tarIndex], ...updateContent };
+    const status = await tuitsDao.updateTuit(tarTuitId, updateContent);
+    res.sendStatus(status);
 };
 
 /**
  * Delete the tuit with the specified tid parameter in the URL
  */
-const deleteTuit = (req, res) => {
+const deleteTuit = async (req, res) => {
     const tarTuitId = req.params.tid; // retrieve the ID of the tuit we want to remove
-    tuits = tuits.filter((tuit) => tuit._id !== tarTuitId); // // filter out the tuit from the tuits array
-    res.sendStatus(200);
+    // no longer using array
+    // tuits = tuits.filter((tuit) => tuit._id !== tarTuitId); // // filter out the tuit from the tuits array
+    const status = await tuitsDao.deleteTuit(tarTuitId); // status reports success or failure
+    res.sendStatus(status);
 };
 
 const TuitsController = (app) => {
