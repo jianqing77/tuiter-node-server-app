a92d197e8846d944817a3e04e757aeb3dd592f9d
Jianiqng Ma <jianqing997@gmail.com>
2023-04-08 22:30:43 -0400

    a9 Mongo sp23

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const CONNECTION_STRING =
     process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
 // connect to the local host machine tuiter database
 mongoose.connect(CONNECTION_STRING);
+console.log('connection string: ' + CONNECTION_STRING);
 
 // execute the request and save return value to a var app
 const app = express();
